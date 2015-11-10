89006ec8ef61769f9254c496a7f284a8f29f17ff
sarbogast <sarbogast@somalogic.com>
2015-11-10 16:17:22 -0700

    fooling with takeWhile

--- exercises/src/test/scala/fpinscala/laziness/LazinessSuite.scala
+++ exercises/src/test/scala/fpinscala/laziness/LazinessSuite.scala
@@ -23,7 +23,8 @@ class LazinessSuite extends FunSuite {
   }
 
   test ("Stream(1,2,3,4).exists(_ == 3"){
-      assert(Stream(1, 2, 3, 4).exists( _ == 3) === true)
+    assert(Stream(1, 2, 3, 4).exists( _ == 2) === true)
+    assert(Stream(1, 2, 3, 4).exists( _ == 3) === true)
   }
 
   test ("Stream(1,2,3,4).exists(_ == 5"){
@@ -52,8 +53,10 @@ class LazinessSuite extends FunSuite {
 
 
   test ("Stream(1,2,3,4).takeWhile( _ > 2)"){
-    assert(Stream(1,2,3,4).takeWhile(_ > 2).exists(_ == 3) === true)
-    assert(Stream(1,2,3,4).takeWhile(_ > 2).exists(_ == 4) === true)
-    println((Stream(1,2,3,4).takeWhile(_ == 2).toList))
+    //assert(Stream(1,2,3,4).takeWhile(_ > 2).exists(_ == 3) === true)
+    //assert(Stream(1,2,3,4).takeWhile(_ > 2).exists(_ == 4) === true)
+    //assert(Stream(1,2,3,4).takeWhile(_ > 2).exists(_ == 2) === false)
+    //println(Stream(1,2,3,4).takeWhile(_ < 2).toList)
+    println(Stream(1,2,3,4).takeWhile(_ > 2).toList)
   }
 }
