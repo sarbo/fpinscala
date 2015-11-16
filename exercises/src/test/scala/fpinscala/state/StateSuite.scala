22119a168ec36f8ffec522c2773bb64f7861f27b
sarbogast <sarbogast@somalogic.com>
2015-11-16 11:20:21 -0700

    test RNG.Simple

--- exercises/src/test/scala/fpinscala/state/StateSuite.scala
+++ exercises/src/test/scala/fpinscala/state/StateSuite.scala
@@ -7,6 +7,7 @@ package fpinscala.state
   * Time: 9:49 AM
   */
 
+import fpinscala.state.RNG.Simple
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -14,17 +15,21 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class StateSuite extends FunSuite {
 
-  test("try it") {
+  test("RNG.Simple(42)") {
 
     val rng = RNG.Simple(42)
 
     val n1 = rng.nextInt
-    println("n1 : " + n1)
+    assert(n1._1  === 16159453)
+    println("n1 : " + n1._1)
 
     val (n2, rng1) = rng.nextInt
+    assert(n2  === 16159453)
     println("n2 : " + n2)
 
-
-    println(rng1.nextInt)
+    val (n3,rng2) = rng1.nextInt
+    assert(n3 === -1281479697)
+    assert(rng2.toString === "Simple(197491923327988)")
+    println("n3: " + n3 + " rng2: " + rng2)
   }
 }
